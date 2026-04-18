7f8512fe5289d0366295966d581768d9f94daef0
Santhosimahalakshmi <santhosimahalakshmi2@gmail.com>
2026-04-18 19:27:58 +0530

    changed to sqlite for deploy

--- bookai/settings.py
+++ bookai/settings.py
@@ -113,14 +113,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-DATABASES = {
+DATABASES = DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bookai_db',
-        'USER': 'root',
-        'PASSWORD': 'santhosi5.3',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 CORS_ALLOW_ALL_ORIGINS = True
