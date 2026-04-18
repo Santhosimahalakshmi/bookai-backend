190d93ea87af28c73401cf4aca9271c357d4231e
Santhosimahalakshmi <santhosimahalakshmi2@gmail.com>
2026-04-18 19:53:45 +0530

    fixed allowed hosts

--- bookai/settings.py
+++ bookai/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-l(3dxzgf7)n(p_13*gp2=u_bnfwzhv09lw=6#$)@#_a)m#1%s1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bookai-backend-2.onrender.com']
 
 
 # Application definition
